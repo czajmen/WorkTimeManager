184ded312cf103694789a407fd361cfd4b162416
czajmen <kamil.czaja94@gmail.com>
2016-06-25 21:18:12 +0200

    poprawa działania raportów pracowniczych

--- WorkTimeManager/WorkerRaportResult.Designer.cs
+++ WorkTimeManager/WorkerRaportResult.Designer.cs
@@ -33,6 +33,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.worker = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.totalwork_label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TotalWork = new System.Windows.Forms.Label();
+            this.mostdep = new System.Windows.Forms.Label();
+            this.mostDayDate = new System.Windows.Forms.Label();
+            this.overhours = new System.Windows.Forms.Label();
+            this.MostDayTime = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -81,10 +91,108 @@
             this.dataGridView1.Size = new System.Drawing.Size(442, 179);
             this.dataGridView1.TabIndex = 8;
             // 
+            // totalwork_label
+            // 
+            this.totalwork_label.AutoSize = true;
+            this.totalwork_label.Location = new System.Drawing.Point(28, 91);
+            this.totalwork_label.Name = "totalwork_label";
+            this.totalwork_label.Size = new System.Drawing.Size(160, 13);
+            this.totalwork_label.TabIndex = 9;
+            this.totalwork_label.Text = "W danym okresie przepracował:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Najwięcej szasu pracował w dziale:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(293, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Najdłużej pracował w dniu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(502, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "godzin";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(293, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Nadgodziny;";
+            // 
+            // TotalWork
+            // 
+            this.TotalWork.AutoSize = true;
+            this.TotalWork.Location = new System.Drawing.Point(205, 91);
+            this.TotalWork.Name = "TotalWork";
+            this.TotalWork.Size = new System.Drawing.Size(35, 13);
+            this.TotalWork.TabIndex = 14;
+            this.TotalWork.Text = "label8";
+            // 
+            // mostdep
+            // 
+            this.mostdep.AutoSize = true;
+            this.mostdep.Location = new System.Drawing.Point(205, 130);
+            this.mostdep.Name = "mostdep";
+            this.mostdep.Size = new System.Drawing.Size(35, 13);
+            this.mostdep.TabIndex = 15;
+            this.mostdep.Text = "label9";
+            // 
+            // mostDayDate
+            // 
+            this.mostDayDate.AutoSize = true;
+            this.mostDayDate.Location = new System.Drawing.Point(449, 91);
+            this.mostDayDate.Name = "mostDayDate";
+            this.mostDayDate.Size = new System.Drawing.Size(10, 13);
+            this.mostDayDate.TabIndex = 16;
+            this.mostDayDate.Text = " ";
+            // 
+            // overhours
+            // 
+            this.overhours.AutoSize = true;
+            this.overhours.Location = new System.Drawing.Point(378, 130);
+            this.overhours.Name = "overhours";
+            this.overhours.Size = new System.Drawing.Size(0, 13);
+            this.overhours.TabIndex = 17;
+            // 
+            // MostDayTime
+            // 
+            this.MostDayTime.AutoSize = true;
+            this.MostDayTime.Location = new System.Drawing.Point(546, 91);
+            this.MostDayTime.Name = "MostDayTime";
+            this.MostDayTime.Size = new System.Drawing.Size(0, 13);
+            this.MostDayTime.TabIndex = 18;
+            // 
             // DayWorkerRaportResult
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.MostDayTime);
+            this.Controls.Add(this.overhours);
+            this.Controls.Add(this.mostDayDate);
+            this.Controls.Add(this.mostdep);
+            this.Controls.Add(this.TotalWork);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.totalwork_label);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.worker);
             this.Controls.Add(this.label1);
@@ -105,5 +213,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label worker;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label totalwork_label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label TotalWork;
+        private System.Windows.Forms.Label mostdep;
+        private System.Windows.Forms.Label mostDayDate;
+        private System.Windows.Forms.Label overhours;
+        private System.Windows.Forms.Label MostDayTime;
     }
 }
--- WorkTimeManager/WorkerRaportResult.cs
+++ WorkTimeManager/WorkerRaportResult.cs
@@ -29,21 +29,199 @@ namespace WorkTimeManager
             string[] tmpdate = date.Split(' ');
             string[] tmpworker = UserForRaport.Split(' ');
 
-            data.Text = date;        
+            List<string> tmp = new List<string>();  //Lista do dnia w ktorym przepracował najwiecej godzin
+                
             worker.Text = UserForRaport;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
 
-
-            if(which==2)
+            if(which==2)  //Dzienny
             {
+         
+                
+                dataGridView1.DataSource = GetDataGrid(tmpworker[0], tmpworker[1], tmpdate[2], tmpdate[1], tmpdate[0]);         
+                data.Text = tmpdate[0] + " " + tmpdate[1] + " " + tmpdate[2];
+                TotalWork.Text = GetAllWordHours(tmpdate[0], tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+                mostdep.Text = mostWorkedDepartment(tmpdate[0], tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+                overhours.Text = GetUserOverHours(tmpdate[0], tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);       
+               
 
-                MessageBox.Show(tmpdate[0] + tmpdate[2]);
-                dataGridView1.DataSource = GetDataGrid(tmpworker[0], tmpworker[1], tmpdate[2], tmpdate[1], tmpdate[0]);
+                //nie ma sensu wyświetlać najdłużej przepracowanego dnia skoro generujemy raport tylko z jednego dnia
+                label4.Hide();
+                label6.Hide();
+
+               
+        
             }
-            else if (which==1)
+            else if (which==1)  //Miesięczny 
             {
                 dataGridView1.DataSource = GetDataGrid(tmpworker[0], tmpworker[1], tmpdate[2], tmpdate[1], null);
+                data.Text = tmpdate[1] +" "+ tmpdate[2];
+                TotalWork.Text = GetAllWordHours(null, tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+                mostdep.Text = mostWorkedDepartment(null, tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+                overhours.Text = GetUserOverHours(null, tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+
+             //   tmp = GetMostWorkedDay(null, tmpdate[1], tmpdate[2], tmpworker[0], tmpworker[1]);
+
+                mostDayDate.Text = "0";
+                MostDayTime.Text = "1";
+                    
+           }         
+        }
+
+
+        private string GetUserOverHours(string day, string month, string year, string name, string surname)
+        {
+
+            try
+            {         
+                string result;
+
+                    month = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture).Month.ToString();         
+                    double worktime = Convert.ToInt16(TotalWork.Text);
+                    worktime = (worktime/60) - 8;
+
+                    if (worktime > 0)
+                    {
+                        result = worktime.ToString();
+                    }
+                    else
+                    {
+                        result = "0";
+                    }
+
+                return result;
+
             }
-            
+            catch
+            {
+                MessageBox.Show("Wystąpił nieznany bład!");
+                return "0";
+            }
+
+        
+        }
+
+        private List<string> GetMostWorkedDay(string day, string month, string year, string name, string surname)  //Poprawić
+        {
+            string DateMostWorkedDay;
+            string hoursCount;
+            List<string> result = new List<string>();
+
+            try
+            {
+                string queryText = string.Format("select w.data from worklist w join users u on w.userID = u.ID where day(w.data)='{0}' and month(w.data)='{1}' and year(w.data)='{2}' and u.name='{3}' and u.surname='{4}' order by sum(w.how_long) limit 1", day, month, year, name, surname);
+
+                 DataBaseControl.OpenConnection(conn);
+
+                List<string> tmpresult = DataBaseControl.Select(conn, queryText);
+                DateMostWorkedDay = DataBaseControl.ResultToString(tmpresult);
+
+                hoursCount = GetAllWordHours(day, month, year, name, surname);
+
+                result.Add(DateMostWorkedDay);
+                result.Add(hoursCount);
+
+
+
+                return result;
+            }
+            catch (MySqlException myexc)
+            {
+
+                MessageBox.Show(myexc.Message);
+                return null;
+            }
+            finally
+            {
+                DataBaseControl.CloseConnection(conn);
+            }
+
+        }
+
+
+        private string mostWorkedDepartment(string day, string month, string year, string name, string surname)
+        {
+            try
+            {
+                string queryText;
+
+                month = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture).Month.ToString();
+                DataBaseControl.OpenConnection(conn);
+                if (string.IsNullOrEmpty(day))
+                {
+                     queryText = string.Format("Select d.name from worklist w join users u on w.userID=u.ID join departments d on d.ID=w.departamentID where u.name='{0}' and  u.surname='{1}' and year(w.data)='{2}' and month(w.data)='{3}' order by sum(w.how_long) limit 1", name, surname, year, month);
+                    
+                }
+                else
+                {
+                    queryText = string.Format("Select d.name from worklist w join users u on w.userID=u.ID join departments d on d.ID=w.departamentID where u.name='{0}' and  u.surname='{1}' and year(w.data)='{2}' and month(w.data)='{3}' and day(w.data)='{4}' order by sum(w.how_long) limit 1", name, surname, year, month,day);
+                }
+
+                List<string> time = new List<string>();
+                time = DataBaseControl.Select(conn, queryText);
+
+
+                if (string.IsNullOrEmpty(time[0]) || string.IsNullOrWhiteSpace(time[0]))
+                {
+                    time[0] = "Brak Danych";
+                }
+
+                return time[0];
+            }
+            catch (MySqlException myexc)
+            {
+
+                MessageBox.Show(myexc.Message);
+                return null;
+            }
+            finally
+            {
+                DataBaseControl.CloseConnection(conn);
+            }
+        }
+
+        private string GetAllWordHours(string day, string month, string year, string name, string surname)
+        {
+
+            try
+            {
+                string queryText;
+                month = DateTime.ParseExact(month, "MMMM", CultureInfo.CurrentCulture).Month.ToString();
+                DataBaseControl.OpenConnection(conn);
+
+                MessageBox.Show(month + " " + year);
+
+                if(!string.IsNullOrEmpty(day))
+                {
+                     queryText = string.Format("Select sum(w.how_long) from worklist w join users u on w.userID=u.ID where u.name='{0}' and  u.surname='{1}' and year(w.data)='{2}' and month(w.data)='{3}' and day(w.data)='{4}' ", name, surname, year, month, day); 
+                }
+                else
+                {
+                     queryText = string.Format("Select sum(w.how_long) from worklist w join users u on w.userID=u.ID where u.name='{0}' and  u.surname='{1}' and year(w.data)='{2}' and month(w.data)='{3}'", name, surname, year, month);
+                }
+             
+                List <string> time = new List<string>();
+                time= DataBaseControl.Select(conn,queryText);
+
+                if (string.IsNullOrEmpty(time[0]) || string.IsNullOrWhiteSpace(time[0]))
+                {
+                    time[0] = "0";
+                }
+
+                return time[0];
+            }
+            catch (MySqlException myexc)
+            {
+
+                MessageBox.Show(myexc.Message);
+                return null;
+            }
+            finally
+            {
+                DataBaseControl.CloseConnection(conn);
+            }
+
         }
 
    
